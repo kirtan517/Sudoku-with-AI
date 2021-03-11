9d7022e72669941261e38bd1de6284ea240cc95b
kirtan517 <kananikirtan73@gmail.com>
2021-03-11 11:40:12 +0530

    color

--- Sudoku.py
+++ Sudoku.py
@@ -1,11 +1,12 @@
 import pygame
 import math
+import time
 
 pygame.init()
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+BLUE = (173, 216, 230)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -30,12 +31,18 @@ def Background(size,width_1,width_2):
     WINDOW_SIZE=BOX_SIZE*9+INNER_LINE*6+BORDER_LINE*2
 
 
-
 Background(100,5,9)
-
-
-
 win=pygame.display.set_mode((WINDOW_SIZE,WINDOW_SIZE))
+
+def CenterCords(center_cords):
+    center = {}
+    k = -1
+    for i in range(len(center_cords)):
+        if i % 9 == 0:
+            center[center_cords[i][0]]={}
+
+        center[center_cords[i][0]][center_cords[i][1]]=-1
+    return center
 
 def Crodinated_Generator(BOX_SIZE,INNER_LINE,BORDER_LINE):
     cords_black_line=[]
@@ -61,10 +68,13 @@ def Crodinated_Generator(BOX_SIZE,INNER_LINE,BORDER_LINE):
         for j in center_cords:
             center.append((i,j))
 
-    return cords_black_line,cords_red_line,center
+    return cords_black_line,cords_red_line,center,center_cords
 
-cords_black_line,cords_red_line,center_cords=Crodinated_Generator(BOX_SIZE, INNER_LINE, BORDER_LINE)
+cords_black_line,cords_red_line,center_cords,UNIQUE=Crodinated_Generator(BOX_SIZE, INNER_LINE, BORDER_LINE)
 
+
+
+CENTERS=CenterCords(center_cords)
 def draw_Background(win,Box_Size,Window_Size,Border_line,Inner_line):
     win.fill(WHITE)
 
@@ -80,16 +90,31 @@ def draw_Background(win,Box_Size,Window_Size,Border_line,Inner_line):
 
 draw_Background(win,BOX_SIZE,WINDOW_SIZE,BORDER_LINE,INNER_LINE)
 
-
 class Digit:
+
 
     def __init__(self,xpos,ypos,digit):
         self.x=xpos
         self.y=ypos
         self.digit=digit
+        CENTERS[self.x][self.y]=self.digit
 
-    def possible(self,number):
-        pass
+
+    def possible(self):
+        Wrong_Cord=[]
+        for i in UNIQUE:
+            current=(self.x,i)
+            if not self.y == i:
+                if self.digit==CENTERS[self.x][i]:
+                    Wrong_Cord.append(self.x,i)         
+
+        for i in UNIQUE:
+            current=(i,self.y)
+            if not self.x == i:
+                if self.digit==CENTERS[i][self.y]:
+                    Wrong_Cord.append(i,self.y)
+        
+
 
     def value(self):
         return self.digit
@@ -105,24 +130,45 @@ class Digit:
         surface=pygame.Surface((self.width,self.height))
         surface.fill(WHITE)
         win.blit(surface, (self.x-self.width//2, self.y-self.height//2))
+        CENTERS[self.x][self.y]= -1
 
-def position(xpos, ypos):
+
+def Coloring(xpos,ypos,color):
+    surface= pygame.Surface((BOX_SIZE,BOX_SIZE))
+    surface.fill(color)
+    win.blit(surface,(xpos-BOX_SIZE//2+1,ypos-BOX_SIZE//2+1))
+
+def pointercolor(xpos,ypos,apos,bpos):
+    if (apos==xpos and ypos == bpos):
+        Coloring(xpos,ypos,BLUE)
+    else : 
+        Coloring(apos,bpos,WHITE)
+
+
+
+  
+def position(xpos, ypos,apos,bpos):
     for x,y in center_cords:
         if (x-BOX_SIZE//2<xpos and x+BOX_SIZE//2>xpos and y-BOX_SIZE//2<ypos and y+BOX_SIZE//2>ypos):
             return x,y
+    return apos,bpos
+
+def errorColor(x,y,axis):
+    pass
 
 def checkRepeat(digit_typed):
     for (x,y) in digit_typed:
         for (m,n) in digit_typed:
             if m==x and not (y==n):
                 if digit_typed[(x,y)].value()==digit_typed[(m,n)].value():
+
                     print("repeatx")
+                    # errorColor(x,y,0)
             if y == n and not (x == m):
                 if digit_typed[(x, y)].value() == digit_typed[(m, n)].value():
+    
                     print("repeaty")
     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-
-
 
 
 
@@ -135,6 +181,9 @@ def draw(win,digit_typed):
 def main():
     run=True
     digit_typed={}
+    xpos=BOX_SIZE//2
+    ypos=BOX_SIZE//2
+    apos, bpos = xpos, ypos
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -143,7 +192,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     xpos, ypos = pygame.mouse.get_pos()
-                    xpos, ypos = position(xpos, ypos)
+                    xpos, ypos = position(xpos, ypos,apos,bpos)
+                    break
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     pass
@@ -171,9 +221,38 @@ def main():
                     if pygame.K_9 == event.key:              
                         digit_typed[(xpos, ypos)] = (Digit(xpos, ypos, 9))
                     checkRepeat(digit_typed)
+
+        pointercolor(xpos, ypos, apos, bpos)
+        if  not(apos == xpos and bpos==ypos):
+            apos,bpos=xpos,ypos
+
         draw(win, digit_typed)
 
 
-if __name__ == "__main__":
 
-    main()
+if __name__ == "__main__":
+    BOXES={}
+    for i in range(1,10):
+        for j in range(1,10):
+            if j / 6 > 1:
+                try:
+                    BOXES[i+2].append((UNIQUE[i-1],UNIQUE[j-1]))
+                except :
+                    BOXES[i+2]=[]
+                    BOXES[i+2].append((UNIQUE[i-1],UNIQUE[j-1]))
+
+            elif j / 3 > 1:
+                try:
+                    BOXES[i+1].append((UNIQUE[i-1],UNIQUE[j-1]))
+                except :
+                    BOXES[i+1]=[]
+                    BOXES[i+1].append((UNIQUE[i-1],UNIQUE[j-1]))
+
+            else:
+                try:
+                    BOXES[i].append((UNIQUE[i-1],UNIQUE[j-1]))
+                except :
+                    BOXES[i]=[]
+                    BOXES[i].append((UNIQUE[i-1],UNIQUE[j-1]))
+    print(BOXES)
+
